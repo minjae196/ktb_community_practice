d4a975733c913240d5395f1034eed5479cd73172
john kim <alswo986@korea.ac.kr>
2026-07-08 15:13:30 +0900

    BE CICD 파일 작성

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.function.EntityResponse;
 
 @RestController
 public class HealthCheckController {
