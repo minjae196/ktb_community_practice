4d65e25d3cfc6786e80ffc5a701610199f505cef
john kim <alswo986@korea.ac.kr>
2026-07-09 15:26:40 +0900

    AWS SDK v2 변경으로 인한 S3 서비스들 변경

--- src/main/java/com/example/demo/config/S3Config.java
+++ src/main/java/com/example/demo/config/S3Config.java
@@ -1,12 +1,11 @@
 package com.example.demo.config;
 
-
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 public class S3Config {
@@ -14,9 +13,16 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3() {
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(Regions.fromName(region))
+    public S3Client s3Client(){
+        return S3Client.builder()
+                .region(Region.of(region))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
                 .build();
     }
 }
--- src/main/java/com/example/demo/service/S3ImageService.java
+++ src/main/java/com/example/demo/service/S3ImageService.java
@@ -1,13 +1,17 @@
 package com.example.demo.service;
 
-import com.amazonaws.services.s3.AmazonS3;
+
 import com.example.demo.dto.image.ImageUploadResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.File;
 import java.util.UUID;
 
 @Service
@@ -15,11 +19,14 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3ImageService implements ImageService{
 
-    private final AmazonS3 amazonS3;
+    private final S3Client s3Client;
     private final ImageProcessor imageProcessor;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
 
     @Override
     public ImageUploadResponseDto uploadImage(MultipartFile file, String type){
@@ -29,12 +36,26 @@ public class S3ImageService implements ImageService{
         String pngKey = "images/post/" + uuid + ".png";
         String webpKey = "images/post/" + uuid + ".webp";
 
-        amazonS3.putObject(bucketName, pngKey, files.getPngFile());
-        amazonS3.putObject(bucketName, webpKey, files.getWebpFile());
+        uploadFile(files.getPngFile(),pngKey,"image/png");
+        uploadFile(files.getWebpFile(),webpKey,"image/webp");
 
         return new ImageUploadResponseDto(
-                amazonS3.getUrl(bucketName,pngKey).toString(),
-                amazonS3.getUrl(bucketName,webpKey).toString()
+                getPublicUrl(pngKey),
+                getPublicUrl(webpKey)
         );
+    }
+
+    private void uploadFile(File file, String key, String contentType) {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(contentType)
+                .build();
+
+        s3Client.putObject(request, RequestBody.fromFile(file));
+    }
+
+    private String getPublicUrl(String key) {
+        return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + key;
     }
 }
