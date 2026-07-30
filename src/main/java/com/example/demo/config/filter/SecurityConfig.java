aebd1fd4479af9afd3afb0518d7f3891b160c224
john kim <alswo986@korea.ac.kr>
2026-07-30 20:41:59 +0900

    /error 추가

--- src/main/java/com/example/demo/config/filter/SecurityConfig.java
+++ src/main/java/com/example/demo/config/filter/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig {
 
     private static final String[] PUBLIC_ENDPOINTS = {
             "/auth",
-            "/health"
+            "/health",
+            "/error"
     };
 
     @Bean
